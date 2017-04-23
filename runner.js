c9f2a522e7edc7b5b3ad4270dde6116b20b5edfd
Maciej Walaszczyk <walaszczykm@gmail.com>
2017-04-23 17:29:33 +0200

    runner as class

--- runner.js
+++ runner.js
@@ -1,40 +1,44 @@
 var request = require('request');
 var chalk = require('chalk');
 
-var result = [];
-var tasksNumber = 0;
+class Runner {
+    constructor(config){
+        this.config = config;
+        this.result = [];
+
+        console.log(chalk.yellow('runner start'));
+        this.loopOverTasks(config);
+    }
+
+    loopOverTasks(tasks){
+        tasks.forEach((task) => {
+            this.startTest(task);
+        });
+    }
+
+    startTest(task) {
+        request(task.request, (error, response) => {
+            if(!error){
+                this.testResponse(task.response, response);
+            }
+        });
+    }
+
+    testResponse(responseComparer, response) {
+        this.result.push({
+            statusCode: responseComparer.statucCode == response.statucCode
+        });
+
+        if(this.result.length == this.config.length){
+            this.saveResult();
+        }
+    }
+
+    saveResult() {
+        console.log(JSON.stringify(this.result, null, 2));
+    }
+}
 
 module.exports = (config) => {
-    console.log(chalk.yellow('runner start'));
-    tasksNumber = config.length;
-    loopOverTasks(config);
-}
-
-var loopOverTasks = (tasks) => {
-    
-    tasks.forEach((task) => {
-        startTest(task);
-    });
-}
-
-var startTest = (task) => {
-    request(task.request, (error, response) => {
-        if(!error){
-            testResponse(task.response, response);
-        }
-    });
-}
-
-var testResponse = (responseComparer, response) => {
-     result.push({
-        statusCode: responseComparer.statucCode == response.statucCode
-     });
-
-     if(result.length == tasksNumber){
-         saveResult();
-     }
-}
-
-var saveResult = () => {
-    console.log(JSON.stringify(result, null, 2));
+    new Runner(config);
 }
